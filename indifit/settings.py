f696a1927601545dbd87ecfacdd56253441d777b
Neil <neil.smyth1989@hotmail.com>
2021-01-22 19:10:59 +0000

    css and updated settings

--- indifit/settings.py
+++ indifit/settings.py
@@ -124,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = 'staticfiles'
+# added so that css static file working
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static")
+    ]
+
